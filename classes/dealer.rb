2d6957253acf3baf40dbe7428c748f455c5ad02e
kashmii <www.myac@gmail.com>
2023-11-16 17:03:02 +0900

    (9) print_table_informationメソッド実装

--- classes/dealer.rb
+++ classes/dealer.rb
@@ -16,7 +16,7 @@ class Dealer
       table["players"] << player_card
     end
 
-    table["players"]
+    table
   end
 
   def initial_cards(game_mode)
@@ -25,5 +25,21 @@ class Dealer
     elsif game_mode == "poker"
       5
     end
+  end
+
+  # 卓の情報を表示する printTableInformation というメソッドです。
+  # このメソッドは、tableという引数を受け取り、卓にいるプレイヤーの数と
+  # ゲームモードを表示し、各プレイヤーの手札を表示します
+  def self.print_table_information(table)
+    output = "Amount of players: " +  table["players"].length.to_s + "... Game mode: " + table["game_mode"] + "\nAt this table: \n"
+
+    table["players"].each_with_index do |_, i|
+      output += "Player " + (i + 1).to_s + " Hand is: "
+      table["players"][i].each_with_index do |_, j|
+        output += table["players"][i][j].get_card_string + " "
+      end
+      output += "\n"
+    end
+    output
   end
 end
--- main.rb
+++ main.rb
@@ -12,14 +12,15 @@ table1 = Dealer.new.start_game(3, "poker")
 # deck1.shuffle_deck
 
 # puts  "#{table1[0][0].get_card_string} #{table1[0][1].get_card_string}"
-# puts  "#{table1[1][0].get_card_string} #{table1[1][1].get_card_string}"
-# puts  "#{table1[2][0].get_card_string} #{table1[2][1].get_card_string}"
 
-his_hands = ""
-table1[0].each do |card|
-  his_hands << card.get_card_string + " "
-end
-puts his_hands
+# his_hands = ""
+# table1[0].each do |card|
+#   his_hands << card.get_card_string + " "
+# end
+# puts his_hands
+
+puts table1["players"].length.to_s
+puts Dealer.print_table_information(table1)
 
 # deckのインスタンス変数にアクセスして1枚目のカードを取得する方法
 # puts deck1.instance_variable_get(:@deck)[51].get_card_string
